ee6b2bc5f662b94dd2fb6183f9c5f77d17822363
Orestis Pantazos <orestis@pantazos.com>
2025-11-05 15:10:31 +0200

    added new file changes for app

--- obj/Debug/net8.0-windows/CibusMed.WPF.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/CibusMed.WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CibusMed.WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef8b1be2666779c9f5631797600c8da587c279f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CibusMed.WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CibusMed.WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -54,7 +54,7 @@ namespace CibusMed.WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CibusMed.WPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CibusMed.WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
